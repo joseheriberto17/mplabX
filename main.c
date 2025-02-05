ab16deffd9fc59f7aeadb12a0a42e1ea58d5e0fb
The_heri24 <jose.heriberto@hotmail.es>
2025-02-04 22:20:52 -0500

    inicio de proyectoasas

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: marquez_final
+ *
+ * Created on 4 de febrero de 2025, 10:19 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+    return (EXIT_SUCCESS);
+}
+
